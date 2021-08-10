3fdc40d3d1614c85bf5ccd5eaf6102b605ce65bb
jinia91 <creee77@gmail.com>
2021-08-10 21:17:38 +0900

    8.10

--- no1541.java
+++ no1541.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -8,25 +9,34 @@ public class no1541 {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), "-");
 
-		boolean flag = false;
+		int sum = Integer.MAX_VALUE;
 
-//		StringBuilder sb = new StringBuilder();
-		String sb = br.readLine();
-//		sb.append(br.readLine());
+		while (st.hasMoreTokens()) {
 
-		int cursor = 0;
-		
-		while (true) {
+			int tmp = 0;
+			StringTokenizer add = new StringTokenizer(st.nextToken(), "+");
 
-			if (sb.) {
-				
-				int start = sb.indexOf("-");
-				flag = true;
-				sb.insert(start, '(');
+			while (add.hasMoreTokens()) {
+
+				tmp += Integer.valueOf(add.nextToken());
+
 			}
+
+			if (sum == Integer.MAX_VALUE) {
+				sum = tmp;
+
+			} else {
+
+				sum -= tmp;
+
+			}
+
 		}
 
-	}
 
+		System.out.println(sum);
+
+	}
 }
